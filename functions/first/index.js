bc6a4ff5f528a9a9ddbff245ae28f4fb1ed19cae
Alex Merced <willcodeforfood@alexmerced.com>
2021-01-23 11:19:17 -0500

    headers

--- functions/first/index.js
+++ functions/first/index.js
@@ -10,7 +10,7 @@ exports.handler = async function (event, context) {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers": "Content-Type",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     },
     body: JSON.stringify({
       path,
