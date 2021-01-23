74021fdecb0fa370e07b13a996317d3506e37ffd
Alex Merced <willcodeforfood@alexmerced.com>
2021-01-23 11:08:48 -0500

    headers

--- functions/first/index.js
+++ functions/first/index.js
@@ -1,22 +1,23 @@
 //We export the function
-exports.handler = async function(event, context) {
+exports.handler = async function (event, context) {
+  //Fetch request details from event object
+  const { path, httpMethod, headers, queryStringParameters, body } = event;
 
-    //Fetch request details from event object
-    const {path, httpMethod, headers, queryStringParameters, body} = event
-
-    // return some JSON data with a staus of 200
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-      },
-      body: JSON.stringify({
-        path,
-        httpMethod,
-        headers,
-        queryStringParameters,
-        body: JSON.parse(body)
-      })
-    }
-  }
+  // return some JSON data with a staus of 200
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+    },
+    body: JSON.stringify({
+      path,
+      httpMethod,
+      headers,
+      queryStringParameters,
+      body: JSON.parse(body),
+    }),
+  };
+};
